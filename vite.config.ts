bb50a70b2e565056eb6deb2ae74d0fb6305e7784
Jose Sanchez <josesancheztrujillo90@hotmail.com>
2025-10-30 18:19:00 -0500

    Code breakage error when updating page fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+    },
+  },
+  // Esto ayuda a Vite a entender que es una SPA
+  appType: 'spa'
+});
